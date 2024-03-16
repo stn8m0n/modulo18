e093976f0ba18c0b5a5daa307520e7149a8b7c3f
Maicon Santana <msn-maicon@hotmail.com>
2024-03-16 05:58:58 -0300

    18.7 min

--- gruntfile.js
+++ gruntfile.js
@@ -1,95 +1,113 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
     grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
-            less:{
-                development:{
-                    files:{
-                        'dev/styles/main.css': 'src/styles/main.less'
-                    }
-                },
-                production:{
-                    options:{
-                        compress: true,
-                    },
-                    files:{
-                        'dist/main.min.css':'src/styles/main.less'
-                    }
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
-            watch: {
-                less: {
-                    files: ['src/styles**/*.less'], /* [** qlqr pasta / * qlqr arquivo] */
-                    tasks: ['less:development']
+
+            production: {
+                options: {
+                    compress: true,
                 },
-                html: {
-                    files: ['src/index.html'],
-                    tasks: ['replace:dev']
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
                 }
+            }
+        },
+
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
             },
-            replace: {
-                dev: {
-                    options: {
-                        patterns: [
-                            {
-                                match: 'ENDERECO_DO_CSS',
-                                replacement: './styles/main.css'
-                            },
-                            {
-                                match: 'ENDERECO_DO_JS',
-                                replacement: '..src/styles/main.js'
-                            }
-                        ]
-                    },
-                    files: [
+
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
                         {
-                            extend: true,
-                            flatten: true,
-                            src: ['src/index.html'],
-                            dest: 'dev/'
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
                         }
                     ]
                 },
-                dist: {
-                    options: {
-                        patterns: [
-                            {
-                                match: 'ENDERECO_DO_CSS',
-                                replacement: './styles/main.min.css'
-                            }
-                        ]
-                    },
-                    files: [
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+
+            dist: {
+                options: {
+                    patterns: [
                         {
-                            extend: true,
-                            flatten: true,
-                            src: ['prebuild/index.html'],
-                            dest: 'dist/'
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
                         }
                     ]
-                }
-            },
-            htmlmin: {
-                dist: {
-                    options: {
-                        removeComments: true,
-                        collapseWhitespace: true,
-                    },
-                    files: {
-                        'prebuild/index.html': 'src/index.html'
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
                     }
+                ]
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options:{
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html'
                 }
-            },
-            clean: ['prebuild']
-        })
+            }
+        },
 
+        clean: ['prebuild'],
 
+        uglify:{
+            target: {
+                files:{
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    })
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-replace')
+    grunt.loadNpmTasks('grunt-contrib-htmlmin')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+
+    grunt.registerTask('default', ['watch'])
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean',])
 }
