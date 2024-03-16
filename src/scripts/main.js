dd8c855758874e2ebd25843cfe6b56afa421675b
Maicon Santana <msn-maicon@hotmail.com>
2024-03-16 04:51:45 -0300

    Calc01

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function   (){
+    document.getElementById('form-sorteador').addEventListener('submit', function(){
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt (numeroMaximo);
+
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        alert(numeroAleatorio);
+    })
+})
