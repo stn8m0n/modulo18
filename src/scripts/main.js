e38c9c54d8442be18ed7e73d554a942ae8c4d2f7
Maicon Santana <msn-maicon@hotmail.com>
2024-03-17 19:30:37 -0300

    ajuste JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', function   (){
-    document.getElementById('form-sorteador').addEventListener('submit', function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
+        evento.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt (numeroMaximo);
 
         let numeroAleatorio = Math.random() * numeroMaximo;
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+
         alert(numeroAleatorio);
     })
 })
